9960fedfbbbfe856e8d30178755af8958c8ff459
Hitesh <contacthiteshprajapati@gmail.com>
2024-03-14 12:23:32 +0530

    Squashed commit of the following:
    commit ea7f4da8fd43b4403791bd9c76af3108a6fd9929
    Author: Naman Bhalla <namanbhalla1998@gmail.com>
    Date:   Tue Mar 12 09:02:25 2024 +0530
    
        Inheritance
    
    commit 98d2190886f9c5232f0817fc13fd9eb15143f3ea
    Author: Naman Bhalla <namanbhalla1998@gmail.com>
    Date:   Fri Mar 8 09:17:14 2024 +0530
    
        Models, Cardinality
    
    commit f6a4d64068a8a965e7a87d78f76d9e06b51425ad
    Author: Naman Bhalla <namanbhalla1998@gmail.com>
    Date:   Mon Mar 4 09:03:04 2024 +0530
    
        Handling exceptions:
    
    commit 333772fed6e142b608192ce5d375c706e78358b5
    Author: Naman Bhalla <namanbhalla1998@gmail.com>
    Date:   Mon Mar 4 08:03:32 2024 +0530
    
        Get /Products?

--- src/main/java/dev/naman/productservicettsmorningdeb24/controllers/ProductController.java
+++ src/main/java/dev/naman/productservicettsmorningdeb24/controllers/ProductController.java
@@ -1,11 +1,18 @@
 package dev.naman.productservicettsmorningdeb24.controllers;
 
 import dev.naman.productservicettsmorningdeb24.dtos.CreateProductRequestDto;
+import dev.naman.productservicettsmorningdeb24.dtos.ErrorDto;
+import dev.naman.productservicettsmorningdeb24.exceptions.ProductNotFoundException;
 import dev.naman.productservicettsmorningdeb24.models.Product;
 import dev.naman.productservicettsmorningdeb24.services.FakeStoreProductService;
 import dev.naman.productservicettsmorningdeb24.services.ProductService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -19,7 +26,7 @@ public class ProductController {
 //    private ProductService productService2 = new FakeStoreProductService();
 
 
-    public ProductController(ProductService productService,
+    public ProductController(@Qualifier("selfProductService") ProductService productService,
                              RestTemplate restTemplate
     ) {
         this.productService = productService;
@@ -51,18 +58,41 @@ public class ProductController {
     // GET /products/201
 
     @GetMapping("/products/{id}")
-    public Product getProductDetails(@PathVariable("id") Long productId) {
+    public Product getProductDetails(@PathVariable("id") Long productId) throws ProductNotFoundException {
         return productService.getSingleProduct(productId);
     }
 
     @GetMapping("/products")
-    public void getAllProduct() {
+    public ResponseEntity<List<Product>> getAllProducts() throws ProductNotFoundException {
 
+        List<Product> products = productService.getProducts();
+
+//        throw new ProductNotFoundException("Bla bla bla");
+
+        ResponseEntity<List<Product>> response = new ResponseEntity<>(products, HttpStatus.OK);
+        return response;
     }
 
     public void updateProduct() {
 
     }
+
+//    @ExceptionHandler(ProductNotFoundException.class)
+//    public ResponseEntity<ErrorDto> handleProductNotFoundExeption(ProductNotFoundException exception) {
+//
+//        ErrorDto errorDto = new ErrorDto();
+//        errorDto.setMessage(exception.getMessage());
+//
+//        return new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
+////        return null;
+//    }
+
+    // Limited to only the exceptions thrown from this controller
+    // Controller Advices: Global
+
+    // if this controller ever ends up throwing ProductNotFoundException.class
+    // for any reason, don't throw that exception as is.
+    // Instead call this method and return what this method is returning
 }
 
 // allProducts - X
